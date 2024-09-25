03f51ec27dba7d00429f5e959df4d7f467f0533a
ngullotta <nikolaus.gullotta@protonmail.com>
2024-09-25 09:38:34 -0500

    More cleanup on `HarmonicOscillators`
    Add sanity checks in `buy` and `sell`
    Move stride to class prop
    Increase default stride length

--- quantipy/strategies/stochastic.py
+++ quantipy/strategies/stochastic.py
@@ -1,8 +1,6 @@
+import numpy as np
 from blankly import StrategyState
 from blankly.indicators import macd, stochastic_rsi
-
-import numpy as np
-
 from pandas import Series
 
 from quantipy.strategies.simple import SimpleStrategy, event
@@ -13,6 +11,9 @@ class HarmonicOscillators(SimpleStrategy):
     A complex strategy involving the Stochastic RSI, (Regular) RSI and
     the MACD all working in concert.
     """
+
+    # Lookback period for the %K and %D checks
+    stride: int = 5
 
     @event("buy")
     def b(self, price: float, symbol: str, state: StrategyState) -> float:
@@ -31,12 +32,16 @@ class HarmonicOscillators(SimpleStrategy):
             self.data[symbol]["close"]
         )
 
+        # Sanity check, must have `stride` number of points to check
+        lengths = [len(rsi), len(stoch_rsi_K), len(stoch_rsi_D)]
+        if any([length < self.stride for length in lengths]):
+            return False
+
         # Both the %K and %D lines must have been below 20 recently
-        stride: int = 2
         below_20_K: Series = stoch_rsi_K < 20
         below_20_D: Series = stoch_rsi_D < 20
         both_below_20: Series = below_20_K & below_20_D
-        lookback: Series = both_below_20[-stride:]
+        lookback: Series = both_below_20[-self.stride:]
         both_below_20_occurred: bool = lookback.any()
         if not both_below_20_occurred:
             return False
@@ -45,19 +50,26 @@ class HarmonicOscillators(SimpleStrategy):
         if rsi.iloc[-1] < 50:
             return False
 
-        macd_res: np.ndarry = None
-        macd_sig: np.ndarry = None
+        macd_line: np.ndarry = None
+        macd_signal: np.ndarry = None
 
         # MACD Hist is unused
-        macd_res, macd_sig, _ = macd(self.data[symbol]["close"])
+        macd_line, macd_signal, _ = macd(self.data[symbol]["close"])
+
+        # Sanity check, must have `minimum_num_points` or more points
+        # to check. This is so we can calculate the slope correctly
+        minimum_num_points = 5
+        lengths = [len(macd_line), len(macd_signal)]
+        if any([length < minimum_num_points for length in lengths]):
+            return False
 
         # Check for MACD cross to confirm uptrend
-        slope: float = (macd_res[-1] - macd_res[-5]) / 5
-        prev_macd: float = macd_res[-2]
-        curr_macd: float = macd_res[-1]
-        curr_macd_s: float = macd_sig[-1]
-        cross: bool = (slope > 0) and (curr_macd >= curr_macd_s > prev_macd)
-        if not cross:
+        slope: float = (macd_line[-1] - macd_line[-5]) / 5
+        prev_macd: float = macd_line[-2]
+        curr_macd: float = macd_line[-1]
+        curr_macd_s: float = macd_signal[-1]
+        crossing_up: bool = slope > 0 and curr_macd >= curr_macd_s > prev_macd
+        if not crossing_up:
             return False
 
         # Finally ensure that both stochastic lines are not overbought
@@ -74,12 +86,16 @@ class HarmonicOscillators(SimpleStrategy):
             self.data[symbol]["close"]
         )
 
+        # Sanity check, must have `stride` number of points to check
+        lengths = [len(rsi), len(stoch_rsi_K), len(stoch_rsi_D)]
+        if any([length < self.stride for length in lengths]):
+            return False
+
         # Both the %K and %D lines must have been above 80 recently
-        stride: int = 2
         above_80_K: Series = stoch_rsi_K > 80
         above_80_D: Series = stoch_rsi_D > 80
         both_above_80: Series = above_80_K & above_80_D
-        lookback: Series = both_above_80[-stride:]
+        lookback: Series = both_above_80[-self.stride:]
         both_above_80_occurred: bool = lookback.any()
         if not both_above_80_occurred:
             return False
@@ -88,19 +104,28 @@ class HarmonicOscillators(SimpleStrategy):
         if rsi.iloc[-1] > 50:
             return False
 
-        macd_res: np.ndarry = None
-        macd_sig: np.ndarry = None
+        macd_line: np.ndarry = None
+        macd_signal: np.ndarry = None
 
         # MACD Hist is unused
-        macd_res, macd_sig, _ = macd(self.data[symbol]["close"])
+        macd_line, macd_signal, _ = macd(self.data[symbol]["close"])
+
+        # Sanity check, must have `minimum_num_points` or more points
+        # to check. This is so we can calculate the slope correctly
+        minimum_num_points = 5
+        lengths = [len(macd_line), len(macd_signal)]
+        if any([length < minimum_num_points for length in lengths]):
+            return False
 
         # Check for MACD cross to confirm downtrend
-        slope: float = (macd_res[-1] - macd_res[-5]) / 5
-        prev_macd: float = macd_res[-2]
-        curr_macd: float = macd_res[-1]
-        curr_macd_s: float = macd_sig[-1]
-        cross: bool = (slope < 0) and (curr_macd <= curr_macd_s < prev_macd)
-        if not cross:
+        slope: float = (macd_line[-1] - macd_line[-5]) / 5
+        prev_macd: float = macd_line[-2]
+        curr_macd: float = macd_line[-1]
+        curr_macd_s: float = macd_signal[-1]
+        crossing_down: bool = (
+            slope < 0 and curr_macd <= curr_macd_s < prev_macd
+        )
+        if not crossing_down:
             return False
 
         # Finally ensure that both stochastic lines are not oversold
