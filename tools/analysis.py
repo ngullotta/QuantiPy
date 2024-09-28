32c9fa72bdc504255d187c63f9d765e00a53af37
ngullotta <nikolaus.gullotta@protonmail.com>
2024-09-27 20:47:22 -0500

    Add some more metrics to analysis

--- tools/analysis.py
+++ tools/analysis.py
@@ -33,20 +33,29 @@ def main() -> None:  # noqa: C901
         for order in orders:
             symbol = order["symbol"]
             orders_list[symbol].append(
-                {"side": order["side"], "price": order["price"]}
+                {"side": order["side"], "price": order["price"], "size": order["size"]}
             )
 
+        profit, loss = 0, 0
         for symbol, orders in orders_list.items():
             wins, pairs = 0, 0
             for buy, sell in zip(orders[::2], orders[1::2]):
+                buy_p, sell_p, buy_q, sell_q = buy["price"], sell["price"], buy["size"], sell["size"]
                 pairs += 1
                 if buy["price"] < sell["price"]:
+                    profit += (sell_p * sell_q) - (buy_p * buy_q)
                     wins += 1
+                else:
+                    loss += (sell_p * sell_q) - (buy_p * buy_q)
             print(
                 "Win Percentage [%s] ~> (%d%%) => %d wins / %d trades"
                 % (symbol, int((wins / pairs) * 100), wins, pairs)
             )
-
+        print("Total Profit $%.2f" % profit)
+        print("Total Loss $%.2f" % loss)
+        print("Net $%.2f" % (profit + loss))
+        print("Profit factor %.2f" % abs(profit / loss))
+        print("Max Drawdown %.2f%%" % data["metrics"]["max_drawdown"]["value"])
 
 if __name__ == "__main__":
     main()
