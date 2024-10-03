b30de056b601d2d0b809603d9d64abfc35c5faec
ngullotta <nikolaus.gullotta@protonmail.com>
2024-10-02 22:28:02 -0500

    Add first test

---
+++ tests/strategies/test_simple.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import pytest
+
+from blankly import KeylessExchange
+from blankly.data.data_reader import PriceReader
+
+from quantipy.strategies.simple import SimpleStrategy
+
+
+@pytest.fixture(scope="module", autouse=True)
+def exchange() -> None:
+    data = Path(__file__).parent / "data" / "pine_wave_technologies.csv"
+    yield KeylessExchange(price_reader=PriceReader(str(data.resolve()), 'PWT-USD'))
+
+
+def test_simple_strategy_buy(exchange) -> None:
+    signal = False
+    def buy(symbol):
+        nonlocal signal
+        if not signal:
+            signal = True
+            return signal
+        return False
+
+    st = SimpleStrategy(exchange)
+    st.buy = buy
+    st.add_price_event(
+        st.tick,
+        symbol="PWT-USD",
+        resolution="1m",
+        init=st.init,
+    )
+    settings = Path(__file__).parent / "settings.json"
+    st.backtest(to="1d", initial_values={"USD": 500}, GUI_output=False, settings_path=settings)
+    assert signal
+    
