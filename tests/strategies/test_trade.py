bb3be1d56a2205fea44aa136d3e21fd2b4e3b06d
ngullotta <nikolaus.gullotta@protonmail.com>
2024-10-04 12:59:58 -0500

    Add shorting test

--- tests/strategies/test_trade.py
+++ tests/strategies/test_trade.py
@@ -40,6 +40,7 @@ class MockInterface:
 class MockState:
     def __init__(self) -> None:
         self.interface = MockInterface()
+        self.base_asset = "FOO"
 
     def get_exchange_type(self) -> str:
         return "mock"
@@ -47,7 +48,6 @@ class MockState:
 
 def test_trade_manager_order_long():
     state = MockState()
-    state.base_asset = "FOO"
     manager = TradeManager()
     position = manager.order(state.interface.price, "FOO", state)
     assert position.open
@@ -62,6 +62,27 @@ def test_trade_manager_order_long():
     assert position.stop_loss == (position.entry * (1 - stop_loss))
     assert position.take_profit == (
         position.entry * (1 + stop_loss * risk_ratio)
+    )
+
+    assert manager.state.get(state.base_asset) is position
+
+def test_trade_manager_order_short():
+    state = MockState()
+    manager = TradeManager()
+    position = manager.order(state.interface.price, "FOO", state, side="sell")
+
+    assert position.open
+    assert position.symbol == "FOO"
+    assert position.state == TradeState.SHORTING
+    # We mock the price to a fixed $ amount, need to ensure the
+    # entry matches
+    assert position.entry == state.interface.price
+
+    stop_loss = manager.default_stop_loss_pct
+    risk_ratio = manager.default_risk_ratio
+    assert position.stop_loss == (position.entry * (1 + stop_loss))
+    assert position.take_profit == (
+        position.entry * (1 - stop_loss * risk_ratio)
     )
 
     assert manager.state.get(state.base_asset) is position
