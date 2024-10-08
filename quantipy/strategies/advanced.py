741be74dcbf06861e55d4264f9b3ba60f1eb80a3
ngullotta <nikolaus.gullotta@protonmail.com>
2024-10-08 14:34:45 -0500

    More coverage

--- quantipy/strategies/advanced.py
+++ quantipy/strategies/advanced.py
@@ -1,199 +1,9 @@
-import logging
-import math
-from collections import namedtuple
-from enum import IntEnum, auto
-from typing import Dict, Union
-
 from blankly import StrategyState
-from blankly.exchanges.interfaces.exchange_interface import ExchangeInterface
-from blankly.exchanges.orders.market_order import MarketOrder
-from blankly.utils import trunc
 
+from quantipy.position import Position
+from quantipy.state import TradeState
 from quantipy.strategies.simple import SimpleStrategy, event
-
-Position = namedtuple(
-    "Position",
-    ["state", "open", "entry", "stop_loss", "take_profit"],
-)
-
-
-class TradeState(IntEnum):
-    READY_NEXT = auto()
-    SHORTING = auto()
-    LONGING = auto()
-
-
-class TradeManager:
-
-    logger = logging.getLogger()
-
-    def __init__(
-        self,
-        interface: ExchangeInterface,
-        risk_ratio: int = 2,
-    ) -> None:
-        self.interface: ExchangeInterface = interface
-        self.positions: Dict[str, Position] = {}
-
-    def new_position(self, symbol: str, **kwargs) -> Position:
-        self.positions.pop(symbol, None)
-        self.positions[symbol] = Position(**kwargs)
-        return self.positions[symbol]
-
-    def update_position(self, symbol: str, **kwargs) -> Union[Position, None]:
-        if pos := self.positions.get(symbol):
-            self.positions[symbol] = pos._replace(**kwargs)
-            return self.positions[symbol]
-        return None
-
-    @staticmethod
-    def clamp(value: float, _max: float, _min: float) -> float:
-        return max(min(value, _max), _min)
-
-    def get_position(self, symbol: str) -> Union[Position, None]:
-        return self.positions.get(symbol)
-
-    def state(self, symbol: str) -> Union[TradeState, None]:
-        if pos := self.get_position(symbol):
-            return pos.state
-
-    @staticmethod
-    def order_to_str(order: MarketOrder) -> str:
-        data: dict = order.get_response()
-        return "(%s) [%s] %.8f of -> %s" % (
-            int(data["created_at"]),
-            data["side"],
-            data["size"],
-            data["symbol"],
-        )
-
-    def log_order(self, order: MarketOrder) -> None:
-        self.logger.info(self.order_to_str(order))
-
-    def get_quantity(
-        self,
-        price: float,
-        symbol: str,
-        state: StrategyState,
-        pct: float = 0.01,
-        stop_loss: float = 0.05,
-        precision: int = 4,
-    ) -> float:
-        pos: Position = self.get_position(state.base_asset)
-        base: dict = state.interface.account[state.base_asset]
-
-        # This must just be a regular "long" market order. Calculate
-        # the quantity using `Cash = Risk amount / Stop loss percentage`
-        if pos is None or not pos.open:
-            cash: float = self.clamp(
-                (state.interface.cash * pct) / stop_loss,
-                state.interface.cash,
-                0,
-            )
-            return trunc(cash / price, precision)
-
-        # We want to buy back the same amount we borrowed
-        if pos.state == TradeState.SHORTING:
-            return abs(base.available)
-
-        # Otherwise this a regular sell order and we should sell it all
-        return base.available
-
-    def __order_internal(
-        self, symbol: str, side: str, size: float, state: StrategyState
-    ) -> Dict:
-        order: MarketOrder = state.interface.market_order(
-            symbol, side=side, size=size
-        )
-        self.log_order(order)
-        return order.get_response()
-
-    def order(
-        self,
-        price: float,
-        symbol: str,
-        state: StrategyState,
-        side: str = "buy",
-        pct: float = 0.01,
-        stop_loss: float = 0.05,
-        risk_ratio: int = 2,
-    ) -> float:
-        pos: Position = self.get_position(state.base_asset)
-        quantity: float = self.get_quantity(
-            price, symbol, state, pct=pct, stop_loss=stop_loss
-        )
-
-        if not quantity:
-            self.logger.warning(
-                "Attempted to buy invalid quantity %f of %s", quantity, symbol
-            )
-            return 0.0
-
-        if side == "buy":
-            # This is a regular "long" order
-            if pos is None or pos.state == TradeState.READY_NEXT:
-                res: dict = self.__order_internal(
-                    symbol, side, quantity, state
-                )
-                pos: Position = self.new_position(
-                    state.base_asset,
-                    state=TradeState.LONGING,
-                    open=(side == "buy" and res["status"] == "done"),
-                    entry=price,
-                    stop_loss=price * (1 - stop_loss),
-                    take_profit=price * (1 + (stop_loss * risk_ratio)),
-                )
-                self.logger.info("%s", pos)
-                return res["size"]
-
-            # This is a short position buy back now
-            if pos.state == TradeState.SHORTING:
-                res: dict = self.__order_internal(
-                    symbol, side, quantity, state
-                )
-                pos: Position = self.update_position(
-                    state.base_asset,
-                    state=TradeState.READY_NEXT,
-                    open=not (side == "buy" and res["status"] == "done"),
-                    entry=price,
-                    stop_loss=-math.inf,
-                    take_profit=math.inf,
-                )
-                self.logger.info("%s", pos)
-                return res["size"]
-        elif side == "sell":
-            # We're going to short this
-            if pos is None or pos.state == TradeState.READY_NEXT:
-                res: dict = self.__order_internal(
-                    symbol, side, quantity, state
-                )
-                pos: Position = self.update_position(
-                    state.base_asset,
-                    state=TradeState.SHORTING,
-                    open=(side == "sell" and res["status"] == "done"),
-                    entry=price,
-                    stop_loss=price * (1 + stop_loss),
-                    take_profit=price * (1 - (stop_loss * risk_ratio)),
-                )
-                self.logger.info("%s", pos)
-                return res["size"]
-
-            # This is a long position, sell the whole smash
-            if pos.state == TradeState.LONGING:
-                res: dict = self.__order_internal(
-                    symbol, side, quantity, state
-                )
-                pos: Position = self.update_position(
-                    state.base_asset,
-                    state=TradeState.READY_NEXT,
-                    open=not (side == "sell" and res["status"] == "done"),
-                    entry=price,
-                    stop_loss=-math.inf,
-                    take_profit=math.inf,
-                )
-                self.logger.info("%s", pos)
-                return res["size"]
-        return 0.0
+from quantipy.trade import TradeManager
 
 
 class AdvancedStrategy(SimpleStrategy):
@@ -210,48 +20,55 @@ class AdvancedStrategy(SimpleStrategy):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.trade_manager = TradeManager(self.interface)
+        self.manager = TradeManager(
+            default_stop_loss_pct=self.STOP_LOSS_PCT,
+            default_risk_ratio=self.RISK_RATIO,
+        )
 
     @event("tick")
     def take_profit(
         self, price: float, symbol: str, state: StrategyState
     ) -> None:
-        pos: Union[Position, None] = self.trade_manager.get_position(symbol)
-        if pos is None or not pos.open:
+        position: Union[Position, None] = self.manager.state.get(symbol)
+        if position is None or not position.open:
             return
 
-        if pos.state == TradeState.LONGING and price >= pos.take_profit:
-            self.trade_manager.order(price, symbol, state, side="sell")
-        elif pos.state == TradeState.SHORTING and price <= pos.take_profit:
-            self.trade_manager.order(price, symbol, state, side="buy")
+        if (
+            position.state == TradeState.LONGING
+            and price >= position.take_profit
+        ) or (
+            position.state == TradeState.SHORTING
+            and price <= position.take_profit
+        ):
+            self.manager.close(position, state)
 
     @event("tick")
     def stop_loss(
         self, price: float, symbol: str, state: StrategyState
     ) -> None:
-        pos: Union[Position, None] = self.trade_manager.get_position(
-            state.base_asset
-        )
-        if pos is None or not pos.open:
+        position: Union[Position, None] = self.manager.state.get(symbol)
+        if position is None or not position.open:
             return
 
-        if pos.state == TradeState.LONGING and price <= pos.stop_loss:
-            self.trade_manager.order(price, symbol, state, side="sell")
-            return
-        elif pos.state == TradeState.SHORTING and price >= pos.stop_loss:
-            self.trade_manager.order(price, symbol, state, side="buy")
-            return
+        if (
+            position.state == TradeState.LONGING
+            and price <= position.stop_loss
+        ) or (
+            position.state == TradeState.SHORTING
+            and price >= position.stop_loss
+        ):
+            self.manager.close(position, state)
 
         # Move stop loss if price moves in our favor (Trailing Stop)
-        if pos.state == TradeState.LONGING:
-            if price > pos.stop_loss:
-                self.trade_manager.update_position(
+        if position.state == TradeState.LONGING:
+            if price > position.stop_loss:
+                self.manager.state.set(
                     state.base_asset,
                     stop_loss=price * (1 - self.STOP_LOSS_PCT),
                 )
-        elif pos.state == TradeState.SHORTING:
-            if price < pos.stop_loss:
-                self.trade_manager.update_position(
+        elif position.state == TradeState.SHORTING:
+            if price < position.stop_loss:
+                self.manager.state.set(
                     state.base_asset,
                     stop_loss=price * (1 + self.STOP_LOSS_PCT),
                 )
@@ -266,25 +83,25 @@ class AdvancedStrategy(SimpleStrategy):
         if not self.safe(symbol):
             return
 
-        pos: Union[Position, None] = self.trade_manager.get_position(
+        position: Union[Position, None] = self.manager.state.get(
             state.base_asset
         )
 
         # Open order for the first time
-        if pos is None:
+        if position is None:
             if self.buy(symbol):
                 self.run_callbacks("buy", *args)
         # Last position is closed and we can take a long or short
-        elif not pos.open and pos.state == TradeState.READY_NEXT:
+        elif not position.open:
             if self.buy(symbol):
                 self.run_callbacks("buy", *args)
             elif self.sell(symbol):
                 self.run_callbacks("sell", *args)
         # Maybe close our long position
-        elif pos.open and pos.state == TradeState.LONGING:
+        elif position.open and position.state == TradeState.LONGING:
             if self.sell(symbol):
                 self.run_callbacks("sell", *args)
         # Maybe close our short
-        elif pos.open and pos.state == TradeState.SHORTING:
+        elif position.open and position.state == TradeState.SHORTING:
             if self.buy(symbol):
                 self.run_callbacks("buy", *args)
---
+++ tests/strategies/test_advanced.py
@@ -0,0 +1,314 @@
+import math
+from pathlib import Path
+from unittest.mock import MagicMock
+
+import pytest
+from blankly import KeylessExchange, StrategyState
+from blankly.data.data_reader import PriceReader
+from blankly.exchanges.orders.market_order import MarketOrder
+from pandas import read_csv
+
+from quantipy.state import TradeState
+from quantipy.position import Position
+from quantipy.strategies.advanced import AdvancedStrategy
+
+
+def get_one_day_start_end(path: Path) -> tuple:
+    data = read_csv(path)
+    end = int(data["time"].iloc[-1])
+    return end - 86400, end
+
+
+@pytest.fixture(scope="module", autouse=True)
+def data_path() -> Path:
+    yield Path(__file__).parent / "data" / "pine_wave_technologies.csv"
+
+
+@pytest.fixture(scope="module", autouse=True)
+def exchange(data_path) -> None:
+    yield KeylessExchange(
+        price_reader=PriceReader(str(data_path.resolve()), "PWT-USD")
+    )
+
+
+def test_simple_strategy_buy_sell_signal(data_path, exchange) -> None:
+    signal = False
+
+    def buy(symbol):
+        nonlocal signal
+        if not signal:
+            signal = True
+            return signal
+        return False
+
+    st = AdvancedStrategy(exchange)
+    assert not st.buy()
+    st.buy = buy
+    st.add_price_event(
+        st.tick,
+        symbol="PWT-USD",
+        resolution="1m",
+        init=st.init,
+    )
+    settings = Path(__file__).parent / "settings.json"
+    start, end = get_one_day_start_end(data_path)
+    st.backtest(
+        start_date=start,
+        end_date=end,
+        initial_values={"USD": 500},
+        GUI_output=False,
+        settings_path=settings,
+    )
+    assert signal
+
+    signal = False
+
+    def sell(symbol):
+        nonlocal signal
+        if not signal:
+            signal = True
+            return signal
+        return False
+
+    assert not st.sell()
+
+    st.sell = sell
+
+    st.backtest(
+        start_date=start,
+        end_date=end,
+        initial_values={"USD": 500},
+        GUI_output=False,
+        settings_path=settings,
+    )
+
+    assert signal
+
+
+def test_advanced_take_profit(exchange) -> None:
+    st = AdvancedStrategy(exchange)
+    symbol = "FOO"
+    st.manager._order = lambda sym, sid, qty, ste: True
+    position = st.manager.state.new(
+        symbol,
+        open=True,
+        size=1,
+        entry=42,
+        take_profit=100,
+        state=TradeState.LONGING,
+    )
+    state = StrategyState(st, {}, symbol)
+    st.take_profit(101, symbol, state)
+    assert st.manager.state.get(symbol).state == TradeState.CLOSED
+
+    st.manager.state.positions.pop(symbol)
+
+    position = st.manager.state.new(
+        symbol,
+        open=True,
+        size=1,
+        entry=42,
+        take_profit=30,
+        state=TradeState.SHORTING,
+    )
+
+    state = StrategyState(st, {}, symbol)
+    st.take_profit(29, symbol, state)
+    assert st.manager.state.get(symbol).state == TradeState.CLOSED
+
+
+def test_advanced_stop_loss(exchange) -> None:
+    st = AdvancedStrategy(exchange)
+    symbol = "FOO"
+    st.manager._order = lambda sym, sid, qty, ste: True
+    position = st.manager.state.new(
+        symbol,
+        open=True,
+        size=1,
+        entry=42,
+        stop_loss=30,
+        state=TradeState.LONGING,
+    )
+    state = StrategyState(st, {}, symbol)
+    st.stop_loss(29, symbol, state)
+    assert st.manager.state.get(symbol).state == TradeState.CLOSED
+
+    st.manager.state.positions.pop(symbol)
+
+    position = st.manager.state.new(
+        symbol,
+        open=True,
+        size=1,
+        entry=42,
+        stop_loss=50,
+        state=TradeState.SHORTING,
+    )
+
+    state = StrategyState(st, {}, symbol)
+    st.stop_loss(51, symbol, state)
+    assert st.manager.state.get(symbol).state == TradeState.CLOSED
+
+
+def test_advanced_trailing_stop_loss_increments(exchange):
+    st = AdvancedStrategy(exchange)
+    symbol = "FOO"
+    st.manager._order = lambda sym, sid, qty, ste: True
+    position = st.manager.state.new(
+        symbol,
+        open=True,
+        size=1,
+        entry=42,
+        stop_loss=30,
+        state=TradeState.LONGING,
+    )
+    state = StrategyState(st, {}, symbol)
+    new_price = 43
+    st.stop_loss(new_price, symbol, state)
+    position = st.manager.state.get(symbol)
+    assert position.stop_loss == (new_price * (1 - st.STOP_LOSS_PCT))
+
+    st.manager.state.positions.pop(symbol)
+
+    position = st.manager.state.new(
+        symbol,
+        open=True,
+        size=1,
+        entry=42,
+        stop_loss=50,
+        state=TradeState.SHORTING,
+    )
+
+    state = StrategyState(st, {}, symbol)
+    new_price = 41
+    st.stop_loss(new_price, symbol, state)
+    position = st.manager.state.get(symbol)
+    assert position.stop_loss == (new_price * (1 + st.STOP_LOSS_PCT))
+
+
+def test_advanced_avoids_blacklist(exchange) -> None:
+    st = AdvancedStrategy(exchange)
+    symbol = "FOO"
+    st.blacklist.append(symbol)
+    st.data[symbol]["close"] = []
+    assert not st.safe(symbol)
+    state = StrategyState(st, {}, symbol)
+    st.tick(42, symbol, state)
+
+
+def test_advanced_tick_buy(exchange):
+    st = AdvancedStrategy(exchange)
+    symbol = "FOO"
+    st.data[symbol]["close"] = []
+    state = StrategyState(st, {}, symbol)
+
+    st.buy = lambda symbol: True
+
+    def go_long(self, price, symbol, state) -> Position:
+        return self.manager.state.new(
+            symbol,
+            entry=price,
+            state=TradeState.LONGING,
+            open=True
+        )
+
+    st.register_event_callback("buy", go_long)
+
+    # Regular
+    st.tick(42, symbol, state)
+    position = st.manager.state.get(symbol)
+    assert position.open
+    assert position.entry == 42
+    assert position.state == TradeState.LONGING
+
+    st.manager.state.positions.pop(symbol)
+    position = st.manager.state.new(
+        symbol, 
+        open=False, 
+        state=TradeState.CLOSED
+    )
+
+    # Position in past, but closed
+    st.tick(42, symbol, state)
+    position = st.manager.state.get(symbol)
+    assert position.open
+    assert position.entry == 42
+    assert position.state == TradeState.LONGING
+
+
+    # Short buyback
+    def close_short(self, price, symbol, state) -> Position:
+        return self.manager.state.new(
+            symbol,
+            state=TradeState.CLOSED
+        )
+
+    st.callbacks["buy"] = []
+    st.callbacks["tick"] = []
+    st.register_event_callback("buy", close_short)
+
+    st.manager.state.positions.pop(symbol)
+    position = st.manager.state.new(
+        symbol, 
+        open=True, 
+        state=TradeState.SHORTING,
+        entry=42
+    )
+
+    st.tick(42, symbol, state)
+    position = st.manager.state.get(symbol)
+    assert not position.open
+    assert position.state == TradeState.CLOSED
+
+
+def test_advanced_tick_sell(exchange):
+    st = AdvancedStrategy(exchange)
+    symbol = "FOO"
+    st.data[symbol]["close"] = []
+    state = StrategyState(st, {}, symbol)
+
+    st.buy = lambda symbol: False
+    st.sell = lambda symbol: True
+
+    def go_short(self, price, symbol, state) -> Position:
+        return self.manager.state.new(
+            symbol,
+            entry=price,
+            state=TradeState.SHORTING,
+            open=True
+        )
+
+    st.register_event_callback("sell", go_short)
+
+    # Short selling
+    position = st.manager.state.new(
+        symbol, 
+        open=False, 
+        state=TradeState.CLOSED,
+    )
+    st.tick(42, symbol, state)
+    position = st.manager.state.get(symbol)
+    assert position.open
+    assert position.entry == 42
+    assert position.state == TradeState.SHORTING
+
+    # Closing a long
+    def close_long(self, price, symbol, state) -> Position:
+        return self.manager.state.new(
+            symbol,
+            state=TradeState.CLOSED,
+            open=False
+        )
+    st.callbacks["sell"] = []
+    st.callbacks["tick"] = []
+    st.register_event_callback("sell", close_long)
+    st.manager.state.positions.pop(symbol)
+    position = st.manager.state.new(
+        symbol,
+        entry=42,
+        open=True, 
+        state=TradeState.LONGING,
+    )
+    st.tick(42, symbol, state)
+    position = st.manager.state.get(symbol)
+    assert not position.open
+    assert position.state == TradeState.CLOSED
